60503cf1cb1ca7019f9912e349104226840c41e8
Salman <salmamabdurrohman@gmail.com>
2025-06-21 05:54:33 +0700

    ganti route welcome jadi login

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\LaporanController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
